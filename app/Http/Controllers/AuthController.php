763ff23e83a4cabbeea474076b9b286a46bb3880
EC2 Default User <ec2-user@ip-172-31-6-53.ec2.internal>
2022-04-06 16:05:02 +0000

    認証の「formデータを受け取る」まで書いた

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ declare( strict_types = 1 );
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginPostRequest;
 
 class AuthController extends Controller {
     /**
@@ -13,6 +14,20 @@ class AuthController extends Controller {
      */
     public function index() {
         return view( 'index' );
+    }
+    
+    /**
+     * ログイン処理
+     * 
+     */
+    public function login( LoginPostRequest $request ) {
+        // validate済み
+        
+        // データの取得
+        $datum = $request->validated();
+        
+        //
+        var_dump( $datum ); exit;
     }
     
 }
