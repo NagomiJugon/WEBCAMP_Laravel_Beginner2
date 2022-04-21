7b8008c41489870c068d6d0298e86a95c3245685
EC2 Default User <ec2-user@ip-172-31-6-53.ec2.internal>
2022-04-21 12:56:26 +0000

    管理画面のユーザー一覧機能を書いた

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -6,6 +6,16 @@
       <title>ログイン機能付きタスク管理サービス</title>
     </head>
     <body>
+      @auth( 'admin' )
+        <menu label="リンク">
+          <a href="/admin/user/list">ユーザー一覧</a><br>
+          管理画面機能 1<br>
+          管理画面機能 2<br>
+          管理画面機能 3<br>
+          管理画面機能 4<br>      
+          <a href="/admin/logout">ログアウト</a>
+        </menu>
+      @endauth
       @yield( 'contents' )
     </body>
 </html>
---
+++ resources/views/admin/user/list.blade.php
@@ -0,0 +1,20 @@
+@extends( 'admin.layout' )
+
+{{-- メインコンテンツ --}}
+@section( 'contents' )
+    <h1>ユーザー一覧</h1>
+    <table border="1">
+      <tr>
+        <th>ユーザーID</th>
+        <th>ユーザー名</th>
+        <th>タスク件数</th>
+      </tr>
+      @foreach ( $users as $user )
+      <tr>
+        <td>{{ $user->id }}</td>
+        <td>{{ $user->name }}</td>
+        <td>{{ $user->task_num }}</td>
+      </tr>
+      @endforeach
+    </table>
+@endsection( 'contents' )
